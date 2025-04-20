e519c6dfb03cabff77b5026e29de9e0958e13754
Danejw <48073085+Danejw@users.noreply.github.com>
2025-04-20 02:13:42 -1000

    fix: update link text in documentation from "Hub" to "Knolia"

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -324,7 +324,7 @@ export default function DocumentationPage() {
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
                 </svg>
-                Go to the Hub
+                Go to the Knolia
               </Link>
             </motion.div>
             <motion.div
